b7c933905d53fa4930c4d59cbbe1f277a76482bb
erblack <erblack@tripleten.com>
2024-09-21 16:09:35 +0200

    initial

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import './styles.css';
+	import Invitation from '../ui/invitation/invitation.svelte';
+
+	const start = new Date('2024-10-12T16:00:00Z');
+	const easter = new Date('2024-10-04T10:00:00Z');
+</script>
+
+<main>
+	<div class="content">
+		<h1>Battleday</h1>
+		<Invitation {start} {easter} />
+	</div>
+</main>
+
+<style>
+	h1 {
+		font-size: calc(var(--cell-size) * 2);
+		line-height: calc(var(--cell-size) * 2);
+		margin: var(--cell-size) 0;
+		transform: translateY(calc(var(--cell-size) * 0.22));
+		font-variation-settings:
+			'BNCE' 5,
+			'INFM' 100,
+			'SPAC' 50;
+		font-weight: 415;
+	}
+	main {
+		height: 100%;
+		display: grid;
+		grid-template-rows: max-content 1fr;
+	}
+
+	.content {
+		margin-left: calc(round(100vw / 2, var(--cell-size)) - var(--content-width) / 2);
+		width: var(--content-width);
+	}
+
+	@media (max-width: 450px) {
+		.content {
+			margin-left: var(--cell-size);
+		}
+	}
+</style>
---
+++ src/ui/invitation/invitation.svelte
@@ -0,0 +1,68 @@
+<script>
+	import Timer from './timer.svelte';
+	import Links from './links.svelte';
+
+	/**
+	 * @type Date
+	 */
+	export let start;
+	/**
+	 * @type Date
+	 */
+	export let easter;
+</script>
+
+<p class="text">I invite you to my birthday!</p>
+<p class="text">
+	Come on october {start.getDate()} at {start.getHours()}:{String(start.getMinutes()).padStart(
+		2,
+		'0'
+	)} in Belgrade at
+	<a class="pin" href="https://yandex.ru/maps/-/CCUZIMBlkA"> Rajićeva 3 apt 2a</a>
+</p>
+<p class="text">Celebration <Timer {start} waitIcon="starts after" finishedIcon="in progress" /></p>
+
+<p class="text">
+	Easter egg hunt <Timer start={easter} waitIcon="starts after" finishedIcon="has begun" />
+</p>
+<Links />
+
+<style>
+	p {
+		--letters-size: 0.85;
+		font-size: calc(var(--cell-size) * var(--letters-size));
+		margin: var(--cell-size) 0;
+		line-height: var(--cell-size);
+		transform: translateY(calc(var(--cell-size) * (1 - var(--letters-size))));
+		font-weight: 380;
+	}
+
+	p:nth-child(3n) {
+		font-variation-settings:
+			'BNCE' 20,
+			'INFM' 33,
+			'SPAC' 0;
+	}
+
+	p:nth-child(3n + 1) {
+		font-variation-settings:
+			'BNCE' 30,
+			'INFM' 80,
+			'SPAC' 0;
+	}
+
+	p:nth-child(3n + 2) {
+		font-variation-settings:
+			'BNCE' 35,
+			'INFM' 42,
+			'SPAC' 0;
+	}
+
+	.pin::before {
+		content: '📍 ';
+	}
+
+	.pin:hover::before {
+		content: '🇷🇸 ';
+	}
+</style>
